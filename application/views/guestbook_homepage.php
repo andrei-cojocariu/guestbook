6386da395fb13134b4291fc049e4fcfb37e5fd18
Cojo <Cojo@DESKTOP-PLO54QS>
2017-07-31 23:18:08 +0300

    create guest book page

--- application/views/guestbook_homepage.php
+++ application/views/guestbook_homepage.php
@@ -1,9 +1,104 @@
-<?php
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<!-- Apple devices fullscreen -->
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<!-- Apple devices fullscreen -->
+	<meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+	<title>FLAT - Login</title>
 
-echo "tezsting";
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+	<!-- icheck -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/plugins/icheck/all.css">
+	<!-- Theme CSS -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css">
+	<!-- Color CSS -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/themes.css">
+
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url(); ?>js/jquery.min.js"></script>
+
+	<!-- Nice Scroll -->
+	<script src="<?php echo base_url(); ?>js/plugins/nicescroll/jquery.nicescroll.min.js"></script>
+	<!-- Validation -->
+	<script src="<?php echo base_url(); ?>js/plugins/validation/jquery.validate.min.js"></script>
+	<script src="<?php echo base_url(); ?>js/plugins/validation/additional-methods.min.js"></script>
+	<!-- icheck -->
+	<script src="<?php echo base_url(); ?>js/plugins/icheck/jquery.icheck.min.js"></script>
+	<!-- Bootstrap -->
+	<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>js/eakroko.js"></script>
+
+	<!--[if lte IE 9]>
+		<script src="<?php echo base_url(); ?>js/plugins/placeholder/jquery.placeholder.min.js"></script>
+		<script>
+			$(document).ready(function() {
+				$('input, textarea').placeholder();
+			});
+		</script>
+	<![endif]-->
+
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="<?php echo base_url(); ?>img/favicon.ico" />
+	<!-- Apple devices Homescreen icon -->
+	<link rel="apple-touch-icon-precomposed" href="<?php echo base_url(); ?>img/apple-touch-icon-precomposed.png" />
+
+</head>
+
+<body class='login'>
+	<div class="wrapper">
+		<h1>
+			<a href="<?php echo site_url(); ?>">
+				<img src="<?php echo base_url(); ?>img/logo-big.png" alt="" class='retina-ready' width="59" height="49">FLAT</a>
+		</h1>
+		<div class="login-body">
+			<h2>SIGN IN</h2>
+			<form action="<?php echo site_url(); ?>" method='get' class='form-validate' id="test">
+				<div class="form-group">
+					<div class="email controls">
+						<input type="text" name='uemail' placeholder="Email address" class='form-control' data-rule-required="true" data-rule-email="true">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="pw controls">
+						<input type="password" name="upw" placeholder="Password" class='form-control' data-rule-required="true">
+					</div>
+				</div>
+				<div class="submit">
+					<div class="remember">
+						<input type="checkbox" name="remember" class='icheck-me' data-skin="square" data-color="blue" id="remember">
+						<label for="remember">Remember me</label>
+					</div>
+					<input type="submit" value="Sign me in" class='btn btn-primary'>
+				</div>
+			</form>
+			<div class="forget">
+				<a href="#">
+					<span>Forgot password?</span>
+				</a>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-38620714-4']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script');
+		ga.type = 'text/javascript';
+		ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
+	})();
+	</script>
+</body>
+
+</html>
